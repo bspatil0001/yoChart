b0a0b79482c9c387b25e00b988cc6aa6187ecc37
basavanagoudaspatil <basavanagouda@provenlpogic.net>
2017-06-15 12:12:47 +0530

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoChatApp
+ * @description
+ * # yoChatApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yoChatApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoChatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoChatApp
+ */
+angular.module('yoChatApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
